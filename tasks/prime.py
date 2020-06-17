5fcc0a4035782a58bba8f00d9ba0cc59ebdfc91d
Sandy Wright <sandy@truss.works>
2020-06-17 15:02:12 -0500

    MB-2964 Update the tag examples to make a smidge more sense

--- tasks/prime.py
+++ tasks/prime.py
@@ -23,7 +23,7 @@ class PrimeTasks(CertTaskSet):
     Set of the tasks that can be called on the Prime API. Make sure to mark tasks with the `@task` decorator and add
     tags where appropriate to make filtering for custom tests easier. Ex:
 
-    @tag('fetch', 'mtos')
+    @tag('mtos')
     @task
     def fetch_mtos(self):
         self.client.get(prime_path("/move-task-orders"), **self.user.cert_kwargs)
@@ -36,7 +36,7 @@ class SupportTasks(CertTaskSet):
     Set of the tasks that can be called on the Support API. Make sure to mark tasks with the `@task` decorator and add
     tags where appropriate to make filtering for custom tests easier. Ex:
 
-    @tag('status_updates', 'shipments')
+    @tag('updates', 'shipments')
     @task
     def update_mto_shipment_status(self):
         # etc.
