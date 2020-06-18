4ce271d5acbeccfa76f5ab6ede333150f87dc44e
Sandy Wright <sandy@truss.works>
2020-06-18 01:25:47 -0500

    MB-2964 Add OfficeTasks as a proof-of-concept for new approach

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 from .anon import AnonBehavior  # noqa
+from .office import OfficeTasks  # noqa
 from .prime import PrimeTasks, SupportTasks  # noqa
---
+++ tasks/office.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+""" TaskSets and tasks for the Office interface. """
+import logging
+import json
+
+from locust import task
+
+from .base import LoginTaskSet
+
+logger = logging.getLogger(__name__)
+
+
+class OfficeTasks(LoginTaskSet):
+    """
+    Set of tasks that can be called for the MilMove Office interface.
+    """
+
+    def on_start(self):
+        """
+        Creates a login right at the start of the TaskSet and stops task execution if the login fails.
+        """
+        super().on_start()  # sets the csrf token
+
+        resp = self._create_login(user_type="office", session_token_name="office_session_token")
+        if not resp.status_code == 200:
+            self.interrupt()  # if we didn't successfully log in, there's no point attempting the other tasks
+
+    @task
+    def get_user_info(self):
+        """
+        Gets the user info for the currently logged in user.
+        """
+        resp = self.client.get("/internal/users/logged_in")
+        try:
+            json_body = json.loads(resp.content)
+        except json.JSONDecodeError:
+            logger.exception("Non-JSON response")
+        else:
+            logger.info(f"ℹ️ User email: {json_body.get('email', 'None')}")
