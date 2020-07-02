6292aa301e7b982408e79975aff7218e4f7b3c0f
Sandy Wright <sandy@truss.works>
2020-07-02 07:24:58 -0500

    MB-2968 Add docstrings to APIParser

--- utils/base.py
+++ utils/base.py
@@ -29,57 +29,68 @@ class ListEnum(Enum):
 
 
 class APIParser:
-    """
-    """
+    """ Parses a YAML Swagger file to isolate endpoint definitions. """
 
     api_file = ""  # can be a relative path or a url
 
     def __init__(self, api_file=""):
         """
-        :param api_file:
+        Sets the api_file and the parser attributes up for the class.
+        :param api_file: str url, optional
         """
-        if self.api_file and api_file:
-            pass  # todo raise exception can only use one
-
-        if not self.api_file:
+        if api_file:
             self.api_file = api_file
 
-        self.parser = ResolvingParser(api_file)
+        if not self.api_file:
+            raise ImplementationError("There must be an API file set to use the APIParser class.")
+
+        self.parser = ResolvingParser(self.api_file)
 
     def _get_endpoint(self, path, method):
         """
-        :param path:
-        :param method:
-        :return:
+        Given a relative endpoint path and method, returns the dictionary representation of the full swagger definition
+        of the endpoint.
+
+        :param path: str
+        :param method: str
+        :return: dict
         """
         try:
             return self.parser.specification["paths"][path][method]
         except KeyError:
-            return  # todo raise exception and log for bad path, method
+            # todo log
+            raise ImplementationError("Endpoint path or method not found in API.")
         except TypeError:
-            return  # todo raise exception and log for bad parsing structure
+            # todo log
+            raise ImplementationError("Bad API structure, unable to get endpoint.")
 
     def get_request_body(self, path, method):
         """
-        :param path:
-        :param method:
-        :return:
+        Given a relative endpoint path and the HTTP/REST method for the endpoint, returns the dictionary representation
+        of the request body.
+
+        :param path: str
+        :param method: str
+        :return: dict
         """
         endpoint = self._get_endpoint(path, method)
         try:
             # grabbing the first body parameter in the endpoint to work with:
             body = [param for param in endpoint["parameters"] if param["in"] == "body"][0]
-        except IndexError:  # this means we got an empty list - no body! Could be intended though
+        except IndexError:  # this means we got an empty list - no body! Could be intended for this endpoint though
             return {}
 
         return body["schema"]
 
     def get_response_body(self, path, method, status="200"):
         """
-        :param path:
-        :param method:
-        :param status:
-        :return:
+        Given a relative endpoint path, the HTTP/REST method for the endpoint, and an optional status code, returns the
+        dictionary representation of the response body.
+
+        :param path: str
+        :param method: str
+        :param status: str, optional
+        :return: dict
         """
         endpoint = self._get_endpoint(path, method)
         try:
