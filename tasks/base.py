f5a0795ee7957e46dffe0972f056754856c73155
Misty <misty@truss.works>
2020-12-14 10:22:05 -0600

    remove debug line as this is coming from DEVLOCAL_AUTH var in mymove

--- tasks/base.py
+++ tasks/base.py
@@ -27,7 +27,6 @@ def check_response(response, task_name="Task", request=None):
         json_response = json.loads(response.content)
 
     except (json.JSONDecodeError, TypeError):
-        logger.exception("TODO remove debug line: " + response.content)
         logger.exception("Non-JSON response.")
         return None, False
 
