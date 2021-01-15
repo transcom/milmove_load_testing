4606b9c85dc447e962fe1b6919164abbd8edac50
Sandy Wright <sandy@truss.works>
2021-01-14 23:19:07 -0600

    MB-6237 Update MilMoveHostMixin with the cert kwargs logic

--- utils/hosts.py
+++ utils/hosts.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 """ utils/hosts.py is for the tools that handle setting the MilMove hostnames and setting up TLS certs. """
 import logging
+import os
+from typing import Optional
 
 from .base import ImplementationError, ListEnum
+from .constants import LOCAL_MTLS_CERT, LOCAL_MTLS_KEY, DOD_CA_BUNDLE, STATIC_TLS_FILES
 
 logger = logging.getLogger(__name__)
 
@@ -29,7 +32,7 @@ class MilMoveDomain(ListEnum):
 
         return self.value
 
-    def host_name(self, env, is_api=False, port="3000", protocol="https"):
+    def host_name(self, env, is_api=False, port="0000", protocol="https"):
         """
         Returns the host name for this domain based on the environment, whether or not it is in the API domain, and the
         port and protocol (for local envs).
@@ -46,7 +49,7 @@ class MilMoveDomain(ListEnum):
             raise ImplementationError("The environment for determining the host name must be included in MilMoveEnv.")
 
         if env == MilMoveEnv.LOCAL.value:
-            port = str(port)  # in case an int was passed in
+            port = str(port)  # just in case an int was passed in
             if not port.isdigit() or len(port) != 4:
                 raise ImplementationError("The local port must be a string of 4 digits.")
 
@@ -61,12 +64,25 @@ class MilMoveHostMixin:
     Mixin for Locust's HttpUser class that sets a host value based on the environment passed in as the --host flag.
     """
 
-    domain = MilMoveDomain.MILMOVE  # any MilMoveDomain value
-    local_protocol = "https"
-    local_port = "8080"
-    is_api = False  # if True, defaults to using the api.<env>.move.mil domain for deployed envs
+    # The abbreviated domain name for the host. Can be any of the values in MilMoveDomain.
+    domain: MilMoveDomain = None
 
-    cert_kwargs = None  # TODO
+    # The abbreviated name of the environment the host is running in (ex. locally or deployed).
+    # Can be any of the values in MilMoveEnv.
+    env: MilMoveEnv = None
+
+    # The HTTP protocol and port used for a local host:
+    local_protocol: str = "https"
+    local_port: str = "0000"
+
+    # A boolean indicating whether or not the host belongs to an API on the server.
+    # For deployed environments only, this will change the host to use api.<env>.move.mil instead of the standard
+    # domain name.
+    is_api: bool = False
+
+    # The set of kwargs that will be used to authenticate an HTTP request, in the format:
+    # {"cert": <cert/key file path(s)>, "verify": <False or the CA bundle file path>}
+    cert_kwargs: Optional[dict] = None
 
     def __init__(self, *args, **kwargs):
         """
@@ -76,19 +92,117 @@ class MilMoveHostMixin:
         # Check if the host value is one of our accepted environments, then set the correct hostname:
         if MilMoveEnv.validate(self.host):
             self.env = self.host  # preserve the original environment value that was passed in on the command line
-            self.host = self.set_host_name()  # set the actual host based on the env
+            self.set_host_name()  # set the actual host based on the env
 
         # If the host was not a MilMoveEnv value, we'll continue with the host the user entered as-is and skip all
         # customization of the hostname
 
         super().__init__(*args, **kwargs)
 
-    def set_host_name(self):
+    @classmethod
+    def set_mil_move_env(cls, env: str):
         """
-        Gets the host name based on the domain, environment, and API status.
+        Sets the environment attribute for the class. Takes in a string and sets a MilMoveEnv literal to cls.env.
         """
-        if not MilMoveDomain.validate(self.domain):
-            logger.debug(f"Bad domain value: {self.domain}")
+        # Check if we already have a value set (this would be purposeful):
+        if cls.env:
+            return
+
+        try:
+            cls.env = MilMoveEnv.match(env)
+        except IndexError:  # means MilMoveEnv could not find a match for the value passed in
+            logger.debug(f"Bad env value: {env}")
+            raise ImplementationError("Environment for MilMoveHostMixin must match one of the values in MilMoveEnv.")
+
+    @classmethod
+    def set_host_name(cls):
+        """
+        Sets the hostname based on the domain, environment, and whether or not it is an API.
+
+        Applies to every instance of the class (or subclass), which reduces the number of times this value will be
+        calculated for users with the exact same attributes to only once during a load test.
+        """
+        # Check if we already have a value set (this would be purposeful):
+        if cls.host:
+            return
+
+        try:
+            cls.host = MilMoveDomain.match(cls.domain).host_name(
+                cls.env.value, cls.is_api, cls.local_port, cls.local_protocol
+            )
+        except IndexError:  # means MilMoveDomain could not find a match for the value passed in
+            logger.debug(f"Bad domain value: {cls.domain}")
             raise ImplementationError("Domain for MilMoveHostMixin must match one of the values in MilMoveDomain.")
 
-        return MilMoveDomain.match(self.domain).host_name(self.env, self.is_api, self.local_port, self.local_protocol)
+    @classmethod
+    def set_cert_kwargs(cls):
+        """
+        Sets the certificate kwargs that will be used for validating the HTTPS request from this user. These will point
+        to the file paths for the TLS cert/key files and the DoD's CA bundle file. The files used will change based on
+        the environment.
+
+        Applies to every instance of the class (or subclass), which reduces the number of times this value will be
+        calculated for users with the exact same attributes to only once during a load test.
+        """
+        # Check if we already have a value set (this would be purposeful) and skip the logic to create it again:
+        if cls.cert_kwargs:
+            return
+
+        if cls.env == MilMoveEnv.LOCAL:
+            cls.cert_kwargs = {"cert": (LOCAL_MTLS_CERT, LOCAL_MTLS_KEY), "verify": False}
+            return
+
+        # We now know we're in a deployed environment, so let's make a deployed cert/key file:
+        cert_key = cls._create_deployed_cert_file()
+
+        # We also need to use the DoD's specific CA bundle for SSL verification in deployed envs:
+        cls.cert_kwargs = {"cert": cert_key, "verify": DOD_CA_BUNDLE}
+
+    @classmethod
+    def _create_deployed_cert_file(cls) -> str:
+        """
+        Grabs the certificate and key values for this environment from the relevant environment variables (which must be
+        set for this function to work), and then creates a new .pem file that contains both the certificate and the key
+        TLS request validation. This is only called for deployed MilMove environments.
+
+        :return: str, the path to the newly created cert file
+        """
+        deployed_tls_cert = os.getenv(f"MOVE_MIL_{cls.env.value.upper()}_TLS_CERT")
+        deployed_tls_key = os.getenv(f"MOVE_MIL_{cls.env.value.upper()}_TLS_KEY")
+
+        if not (deployed_tls_cert and deployed_tls_key):
+            logger.debug(f"Unable to find cert and key values for environment: {cls.env.value}")
+            raise ImplementationError(
+                "Cannot run load testing in a deployed environment without the matching certificate and key."
+            )
+
+        cert_key_file = os.path.join(STATIC_TLS_FILES, f"{cls.env.value}_tls_cert_key.pem")
+        with open(cert_key_file, "w") as f:
+            f.write(deployed_tls_cert)
+            f.write("\n")
+            f.write(deployed_tls_key)
+
+        return cert_key_file
+
+    @classmethod
+    def _remove_deployed_cert_file(cls):
+        """
+        Removes the .pem cert/key file that was created for running load tests against a deployed environment.
+        """
+        try:
+            os.remove(cls.cert_kwargs["cert"])
+        except (KeyError, FileNotFoundError):
+            # If there was a KeyError, that means "cert" wasn't in the cert_kwargs dict - it may have been cleard out.
+            # If there was a FileNotFoundError, whatever path was in the "cert" kwarg was incorrect or the file was
+            # already removed.
+
+            # In either case, let's try again to remove the file using the custom filename to be extra sure we're
+            # cleaning up after ourselves:
+            try:
+                os.remove(os.path.join(STATIC_TLS_FILES, f"{cls.env.value}_tls_cert_key.pem"))
+            except FileNotFoundError:
+                # The file is gone, huzzah!
+                pass
+
+        # Finally, clear out all traces of the deployed cert file:
+        cls.cert_kwargs = None
