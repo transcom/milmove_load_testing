4d6d7a5a275030b19d3abf8f4b73a69bee473a86
Felipe Lee <felipe@truss.works>
2021-12-28 15:26:37 -0800

    MB-10533 Add copyright for the code we copied from locust-plugins

--- utils/rest.py
+++ utils/rest.py
@@ -4,6 +4,9 @@ The code in this file helps us out with working with json endpoints and making R
 
 The code in this file is based on `RestUser` from `locust-plugins`:
 https://github.com/SvenskaSpel/locust-plugins/blob/master/locust_plugins/users/rest.py
+
+Copyright 2019 Svenska Spel AB
+From: https://github.com/SvenskaSpel/locust-plugins/blob/master/LICENSE
 """
 import re
 import traceback
