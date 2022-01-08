88bb3ee3b0df20ba3d7268f1cab488a3fc28814d
Felipe Lee <felipe@truss.works>
2022-01-07 16:29:36 -0800

    MB-10977 Convert customer Users/TaskSets to use new classes/helpers

--- locustfiles/milmove.py
+++ locustfiles/milmove.py
@@ -3,20 +3,12 @@
 from locust import HttpUser, between
 
 from tasks import MilMoveTasks
-from utils.hosts import MilMoveDomain, MilMoveHostMixin
 
 
-class MilMoveUser(MilMoveHostMixin, HttpUser):
+class MilMoveUser(HttpUser):
     """
     Tests the MilMove app.
     """
 
-    local_protocol = "http"
-    local_port = "8080"
-
-    deployed_subdomain = "my"
-
-    domain = MilMoveDomain.MILMOVE
-
-    wait_time = between(1, 9)
     tasks = {MilMoveTasks: 1}
+    wait_time = between(1, 9)
