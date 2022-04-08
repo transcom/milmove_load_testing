d248cca3833276410e6d74f9c6b346cb4bc1e95f
Kyle Hill <kyle@truss.works>
2022-04-08 14:55:37 -0400

    Adds explanatory comments

--- tasks/queue.py
+++ tasks/queue.py
@@ -34,7 +34,11 @@ class QueueTaskSet(MilMoveRequestMixin, TaskSet):
 
 class MilMoveHHGQueueTasks(QueueTaskSet):
     """
-    Set of tasks that can be called for the MilMove interface.
+    Creates tasks that traverse the entire flow, from customer through TIO. These tasks largely only differ
+    based on what kind of shipment(s) the customer is adding to their move. Weighted randomness determines how
+    frequently the various customer shipment types are selected.
+
+    See http://docs.locust.io/en/stable/writing-a-locustfile.html#task-decorator for more info
     """
 
     def on_start(self):
@@ -42,16 +46,25 @@ class MilMoveHHGQueueTasks(QueueTaskSet):
 
     @task(2)
     def start_single_hhg_flow(self):
+        """
+        Kicks off a flow where the customer creates a single HHG shipment
+        """
         f = SingleHHGFlow()
         self.start_flow(f)
 
     @task(1)
     def start_double_hhg_flow(self):
+        """
+        Kicks off a flow where the customer creates two seperate HHG shipments
+        """
         f = DoubleHHGFlow()
         self.start_flow(f)
 
     @task(1)
     def start_nts_flow(self):
+        """
+        Kicks off a flow where the customer creates a single NTS shipment
+        """
         f = NTSFlow()
         self.start_flow(f)
 
